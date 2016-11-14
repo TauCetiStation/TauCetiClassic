2c5fdd6994e29a3b7fcc99a7a9d1979ba7652312
Raschert0 <raschert0@gmail.com>
2016-11-15 00:35:45 +0200

    paradise- -> tau-

--- goon/browserassets/js/browserOutput.js
+++ goon/browserassets/js/browserOutput.js
@@ -250,11 +250,11 @@ function setCookie(cname, cvalue, exdays) {
 	var d = new Date();
 	d.setTime(d.getTime() + (exdays*24*60*60*1000));
 	var expires = 'expires='+d.toUTCString();
-	document.cookie = "paradise-" + cname + '=' + cvalue + '; ' + expires;
+	document.cookie = "tau-" + cname + '=' + cvalue + '; ' + expires;
 }
 
 function getCookie(cname) {
-	var name = "paradise-" + cname + '=';
+	var name = "tau-" + cname + '=';
 	var ca = document.cookie.split(';');
 	for(var i=0; i < ca.length; i++) {
 	var c = ca[i];
