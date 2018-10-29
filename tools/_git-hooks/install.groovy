ec06f38ab53e1c4b0e55930f66e30e9ce1cb749f
SpaiR <klimbetoo@gmail.com>
2018-10-29 12:32:57 +0300

    Upd JTGMerge and contributing guide  - JTGMerge 1.2 с пофикшеным мапмержем  - contributing guide и инфой по работе с картами

--- tools/_git-hooks/install.groovy
+++ tools/_git-hooks/install.groovy
@@ -1,6 +1,9 @@
 @Grab(group = 'org.eclipse.jgit', module = 'org.eclipse.jgit', version = '5.0.2.201807311906-r')
 import org.eclipse.jgit.api.Git
 
+println 'Dependencies installed!'
+println 'Installing hooks...'
+
 def git = Git.open(new File('../../.'))
 def config = git.repository.config
 
@@ -13,26 +16,31 @@ new File('.').eachFile { file ->
         case 'merge':
             config.unsetSection('merge', "merge-$cleanFileName")
 
-            def setMergeSection = { subSectionName, line ->
-                config.setString('merge', "merge-$cleanFileName", subSectionName, line)
-            }
-
+            def setMergeSection = { subSectionName, line -> config.setString('merge', "merge-$cleanFileName", subSectionName, line) }
             def lines = file.readLines()
 
             setMergeSection('name', lines.get(0))
             setMergeSection('driver', lines.get(1).replace('{OS_EXT}', getOSExt()).trim())
+
+            println "Merge driver for `$cleanFileName` files installed!"
+
             break
         case 'hook':
             def hookFile = new File("../../.git/hooks/$cleanFileName")
             hookFile.delete()
             hookFile.createNewFile()
             hookFile << file.text
+
+            println "$cleanFileName hook installed!"
+
             break
     }
 }
 
 config.save()
 git.close()
+
+println 'All dependencies and hooks are installed! You may close the window.'
 
 def getOSExt() {
     System.env['OS'].toLowerCase().contains('windows') ? 'bat' : 'sh'
