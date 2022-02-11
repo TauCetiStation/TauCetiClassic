c9fbcb00ccd8ef0efc363e8a7cb99e414b818486
KIBORG04 <bossmira4@gmail.com>
2022-02-11 21:19:03 +0300

    che

--- test/run-test.sh
+++ test/run-test.sh
@@ -179,7 +179,7 @@ match_helper() {
         s=${s#*"${BASH_REMATCH[1]}"}
 		istype="${BASH_REMATCH[2]}"
         s=${s#*"${BASH_REMATCH[2]}"}
-		istype_pattern=`echo "$istype" | sed -r "s/istype\(([A-Za-z]+), ([A-Za-z0-9\/]+)\)/istype\\\\\([A-Za-z]+, \1\\\\\)/"`
+		istype_pattern=`echo "$istype" | sed -r "s/istype\([A-Za-z]+, ([A-Za-z0-9\/]+)\)/istype\\\\\([A-Za-z]+, \1\\\\\)/"`
         run_test_fail "change istype to $define. Use this pattern for your VSCode: ^(?!#define)(.*)$istype_pattern -> \$1$define(\$2)" "grep -RPnr --include='*.dm' '^(?!#define).*$istype_pattern' code/"
     done
 }
