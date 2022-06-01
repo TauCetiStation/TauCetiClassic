f15f7b4b057708216c92eebfd356e11797d0c394
kalazus <kalazus@mail.ru>
2022-06-01 20:38:02 +0300

    fix

--- tgui/packages/tgui/interfaces/CameraConsole.js
+++ tgui/packages/tgui/interfaces/CameraConsole.js
@@ -41,10 +41,12 @@ export const selectCameras = (cameras, searchText = '') => {
 };
 
 export const CameraConsole = (props, context) => {
+  Byond.winget("mapwindow.map", "style").then(style => {
+    Byond.winset(mapRef, "style", style);
+  });
   const { act, data } = useBackend(context);
   const { mapRef, activeCamera } = data;
   const cameras = selectCameras(data.cameras);
-  const mapStyle = Byond.winget("mapwindow.map", "style");
   const [
     prevCameraName,
     nextCameraName,
@@ -84,7 +86,6 @@ export const CameraConsole = (props, context) => {
           params={{
             id: mapRef,
             type: 'map',
-            style: mapStyle,
           }} />
       </div>
     </Window>
