809b455584ee0e5500a86dc1b23b638251022dec
Darth Sidious <andrgysev02@mail.ru>
2023-02-18 01:04:38 +0400

    готово

--- tgui/packages/tgui/index.js
+++ tgui/packages/tgui/index.js
@@ -12,6 +12,7 @@ import './styles/themes/hackerman.scss';
 import './styles/themes/malfunction.scss';
 import './styles/themes/neutral.scss';
 import './styles/themes/ntos.scss';
+import './styles/themes/nanotrasen.scss';
 import './styles/themes/paper.scss';
 import './styles/themes/retro.scss';
 import './styles/themes/syndicate.scss';
