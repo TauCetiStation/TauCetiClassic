a82b2ce349042c92fef08ecc46459c16d47c49ce
Darth Sidious <andrgysev02@mail.ru>
2023-07-18 22:47:47 +0400

    чуток переделал.

--- tgui/packages/tgui/interfaces/CardPay.js
+++ tgui/packages/tgui/interfaces/CardPay.js
@@ -15,8 +15,8 @@ export const CardPay = (props, context) => {
   for (let row = 0; row < 3; row++) {
     for (let col = 1; col < 4; col++) {
       buttons.push(<Button
-        className="cardpay_button_general cardpay_button_variant-normal"
-        content={<Box className="cardpay_button-inside_general cardpay_button-inside_variant-normal">{row * 3 + col}</Box>}
+        className="cardpay_button cardpay_button--normal"
+        content={<Box className="cardpay_button-inside cardpay_button-inside--normal">{row * 3 + col}</Box>}
         onClick={() => act("pressnumber", { number: row * 3 + col })}
       />);
     }
@@ -57,8 +57,9 @@ export const CardPay = (props, context) => {
   return (
     <Window theme=""
       width={220}
-      height={340}>
-      <Window.Content className="cardpay_window">
+      height={340}
+      titleClassName="cardpay_window-titlebar">
+      <Window.Content className="cardpay_window-contents">
         <Box
           className="cardpay_monitor"
         >
@@ -67,18 +68,18 @@ export const CardPay = (props, context) => {
         <Box width="158px" height="208px" position="absolute" left="32px" top="95px">
           {buttons}
           <Button
-            className="cardpay_button_general cardpay_button_variant-red"
-            content={<Box className="cardpay_button-inside_general cardpay_button-inside_variant-red">{"X"}</Box>}
+            className="cardpay_button cardpay_button--red"
+            content={<Box className="cardpay_button-inside cardpay_button-inside--red">{"X"}</Box>}
             onClick={() => act("clearnumbers")}
           />
           <Button
-            className="cardpay_button_general cardpay_button_variant-normal"
-            content={<Box className="cardpay_button-inside_general cardpay_button-inside_variant-normal">{0}</Box>}
+            className="cardpay_button cardpay_button--normal"
+            content={<Box className="cardpay_button-inside cardpay_button-inside--normal">{0}</Box>}
             onClick={() => act("pressnumber", { number: 0 })}
           />
           <Button
-            className="cardpay_button_general cardpay_button_variant-green"
-            content={<Box className="cardpay_button-inside_general cardpay_button-inside_variant-green">{"O"}</Box>}
+            className="cardpay_button cardpay_button--green"
+            content={<Box className="cardpay_button-inside cardpay_button-inside--green">{"O"}</Box>}
             onClick={() => act("approveprice")}
           />
         </Box>
