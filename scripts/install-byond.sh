daa5ca86c8050af9cac9c7e5009495bbccae83cb
KIBORG04 <bossmira4@gmail.com>
2025-05-10 21:34:24 +0300

    Добавил че-то там

--- scripts/install-byond.sh
+++ scripts/install-byond.sh
@@ -8,7 +8,7 @@ else
   mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
   cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
   echo "Installing DreamMaker to $PWD"
-  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "TauCetiStation/1.0 Continuous Integration"
   unzip -o byond.zip
   cd byond
   make here
