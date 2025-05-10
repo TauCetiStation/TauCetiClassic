075694d68f21b1943fd5b6bde77e2ffa5cc75bf4
KIBORG04 <bossmira4@gmail.com>
2025-05-10 21:54:38 +0300

    1

--- scripts/install-byond.sh
+++ scripts/install-byond.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+set -x
 if [ -f "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/DreamMaker" ];
 then
   echo "Using cached directory."
@@ -10,8 +11,10 @@ else
   echo "Installing DreamMaker to $PWD"
   curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "TauCetiStation/1.0 Continuous Integration"
   unzip -o byond.zip
-  ls
-  ls -lrt -d -1 "$PWD"/{*,.*}
   cd byond
   make here
 fi
+
+echo "TESTTESTTESTTESTTESTTEST"
+ls -R $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
+echo "TESTTESTTESTTESTTESTTEST"
