97daee350d146df9db8ce0055a96e1e7223b9d53
maleyvich <104298869+maleyvich@users.noreply.github.com>
2025-06-30 12:12:28 +0500

    dada

--- tgui/packages/tgui/interfaces/Fax.js
+++ tgui/packages/tgui/interfaces/Fax.js
@@ -5,23 +5,21 @@ import { Window } from '../layouts';
 export const SampleInterface = (props, context) => {
   const { act, data } = useBackend(context);
   // Extract `health` and `color` variables from the `data` object.
-  const {
-    scan,
-    color,
-  } = data;
+  const { scan, color } = data;
   return (
     <Window width={300} height={405}>
       <Window.Content>
-      <Section
-        title="Аутентификация в систему"
-        minHeight="82px"
-        buttons={
-          <Button
-            icon="eject"
-            content={scan ? scan.name : '--------'}
-            onClick={() => act('confirm')}
-          />
-        }></Section>
+        <Section
+          title="Аутентификация в систему"
+          minHeight="82px"
+          buttons={
+            <Button
+              icon="eject"
+              content={scan ? scan.name : '--------'}
+              onClick={() => act('confirm')}
+            />
+          }
+        />
       </Window.Content>
     </Window>
   );
